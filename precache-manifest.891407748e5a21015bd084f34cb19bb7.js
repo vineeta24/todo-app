a4cd302b09e92ebdff6016d23abce03cb43aa396
vineeta24 <vineetakande@gmail.com>
2020-06-26 12:10:13 -0300

    Updates

---
+++ precache-manifest.891407748e5a21015bd084f34cb19bb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798aac93ccae767282c1dd5b9f3bc9d6",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "872645f3a26f18246c64",
+    "url": "/todo-app/static/css/main.44c18a81.chunk.css"
+  },
+  {
+    "revision": "78853fa2c580410ce68a",
+    "url": "/todo-app/static/js/2.ff6986c1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-app/static/js/2.ff6986c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "872645f3a26f18246c64",
+    "url": "/todo-app/static/js/main.3cf865ca.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
